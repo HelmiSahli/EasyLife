6649562d610fec8e13dbb98034b5c73ec0651d0b
Helmi <helmi.sahli@esprit.tn>
2017-08-19 18:49:11 +0100

    Initial commit

---
+++ app/src/main/java/summer/application/nice/easylife/activity/PrivacyPolicyActivity.java
@@ -0,0 +1,36 @@
+package summer.application.nice.easylife.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import summer.application.nice.easylife.R;
+
+/**
+ * Created by Helmi on 14/08/2017.
+ */
+public class PrivacyPolicyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_privacy_policy);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == android.R.id.home) {
+            // finish the activity
+            onBackPressed();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
